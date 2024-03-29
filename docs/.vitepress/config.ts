0616e384b9ef15e2067b192b9bfa7692b3a3339d
友人A <niu13259716901@163.com>
2024-03-29 15:17:22 +0800

    feat: classify component documents

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -32,6 +32,31 @@ export default {
           items: [
             { text: "Button 按钮", link: "/components/button/" },
             { text: "Layout 布局", link: "/components/layout/" },
+            { text: "Icon 图标", link: "/components/icon/" },
+          ],
+        },
+        {
+          text: "Form 表单组件",
+          items: [
+            { text: "Checkbox 多选框", link: "/components/checkbox/" },
+            { text: "Input 输入框", link: "/components/input/" },
+            { text: "Select 选择器", link: "/components/select/" },
+          ],
+        },
+        {
+          text: "Data 数据展示",
+          items: [
+            { text: "Calendar 日历", link: "/components/calendar/" },
+            { text: "Carousel 走马灯", link: "/components/carousel/" },
+            { text: "Steps 步骤条", link: "/components/steps/" },
+          ],
+        },
+        {
+          text: "Feedback 反馈组件",
+          items: [
+            { text: "Dialog 对话框", link: "/components/dialog/" },
+            { text: "Message 消息提示", link: "/components/message/" },
+            { text: "Tooltip 文字提示", link: "/components/tooltip/" },
           ],
         },
       ],
